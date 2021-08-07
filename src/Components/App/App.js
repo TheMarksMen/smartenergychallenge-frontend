715ea4336d92b17dd63144940419d3f97c6b4f97
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-08-07 15:44:40 +1200

    🧹 formatted with prettier

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,15 @@
 import logo from './../../logo.svg';
 import './App.css';
-import Button from '@material-ui/core'
+import Button from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
