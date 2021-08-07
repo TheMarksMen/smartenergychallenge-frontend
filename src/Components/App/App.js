638c6f1ec566085bcad1f6b634aafabb77601f20
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-08-07 16:30:47 +1200

    í¾ created first materialui components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,33 @@
-import logo from './../../logo.svg';
-import './App.css';
-import Button from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, makeStyles} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 function App() {
+    const c = useStyles();
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
+        <div className={c.root + "App"}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={c.title}>
+                       Smart Energy Challenge 
+                    </Typography>
+                    <IconButton edge="end" color="inherit" aria-label="profile">
+                        <AccountCircle />
+                    </IconButton>
+                </Toolbar>
+                </AppBar>
         </div>
     );
 }
