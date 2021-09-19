1d7f1bc492a300598d57bc2386da1b377b497e23
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-09-19 18:26:43 +1200

    👓 implemented basic grid layout

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,22 +6,23 @@ import '@fontsource/roboto';
 import Chart from '../Charts/Chart';
 import AreaChart from '../Charts/AreaChartComponent';
 import { powerData } from './Stud';
+import GridLayout from 'react-grid-layout';
 
 function Dashboard() {
+    const layout = [
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+
     return (
         <div className="App">
-            <Grid
-                container
-                justifyContent="center"
-                spacing={5}
-                justify="center"
-                direction="row"
-                style={{ minHeight: '80vh', marginTop: '5vh' }}
-            >
-                <Chart>
-                    <AreaChart data={powerData} />
-                </Chart>
-            </Grid>
+            <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+
+                <div key="c">
+                    <Chart>
+                        <AreaChart data={powerData} />
+                    </Chart>
+                </div>
+            </GridLayout>
         </div>
     );
 }
