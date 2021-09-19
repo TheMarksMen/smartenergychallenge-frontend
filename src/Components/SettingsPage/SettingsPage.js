919c90f5efdeb3980b1a4bd518f9e4515954316e
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-09-19 15:36:31 +1200

    ⚙  Improved settings page

--- src/Components/SettingsPage/SettingsPage.js
+++ src/Components/SettingsPage/SettingsPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Typography } from "@mui/material";
+import { Typography, Box, Grid } from "@mui/material";
 
 export default function SettingsPage() {
     return (
-        <Typography>
-            Settings
-        </Typography>
+        <Grid 
+            container
+            alignItems="center"
+            justifyContent="center"
+            spacing={4}     
+        >
+            <Grid item>
+                <Box 
+                    sx={{
+                        p: 4,
+                        width: '80%'
+                    }} 
+                >
+                    <Typography variant="h3">
+                        Settings
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
