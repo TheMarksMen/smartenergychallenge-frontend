f13484be1138ab2c9ff956ac617bb75f410e33c0
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-09-20 09:42:00 +1200

    🎪 stripped chart to minimal

--- src/Components/Charts/Chart.js
+++ src/Components/Charts/Chart.js
@@ -5,75 +5,10 @@ import { useTheme } from '@mui/styles';
 
 function Chart(props) {
     const theme = useTheme();
-    const [anchor, setAnchor] = React.useState(null);
-    const [option, setOption] = React.useState('Power');
-
-    const handleClick = (event) => {
-        setAnchor(event.currentTarget);
-    };
-
-    const handleListItemClick = (event) => {
-        setOption(event.currentTarget.textContent);
-    };
-
-    const handleClose = () => {
-        setAnchor(null);
-    };
-
     return (
         <Box display="inline-block">
             <Paper style={{ padding: '3em', height: '' }}>
-                <Grid
-                    container
-                    justifyContent="center"
-                    spacing={5}
-                    justify="center"
-                    direction="column-reverse"
-                >
-                    <Button
-                        aria-controls="simple-menu"
-                        aria-haspopup="true"
-                        onClick={handleClick}
-                    >
-                        Select Value
-                    </Button>
-                    { props.children } 
-                    <Menu
-                        id="menu"
-                        anchorEl={anchor}
-                        keepMounted
-                        open={Boolean(anchor)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem
-                            onClick={(e) => {
-                                handleClose(e);
-                                handleListItemClick(e);
-                            }}
-                        >
-                            Power
-                        </MenuItem>
-                        <MenuItem
-                            onClick={(e) => {
-                                handleClose(e);
-                                handleListItemClick(e);
-                            }}
-                        >
-                            Peak Voltage
-                        </MenuItem>
-                        <MenuItem
-                            onClick={(e) => {
-                                handleClose(e);
-                                handleListItemClick(e);
-                            }}
-                        >
-                            RMS Current
-                        </MenuItem>
-                    </Menu>
-                    <Typography align="center" variant="h4">
-                        {option}
-                    </Typography>
-                </Grid>
+                { props.children }   
             </Paper>
         </Box>
     );
